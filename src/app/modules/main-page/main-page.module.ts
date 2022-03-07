199276e25263f73c8e0dd989318d54a3c47ea8cf
Alejo <rafael.valencia.r4@gmail.com>
2022-03-07 00:33:09 -0500

    configuracion bootstrap y primeng

--- src/app/modules/main-page/main-page.module.ts
+++ src/app/modules/main-page/main-page.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainBodyComponent } from './components/main-body/main-body.component';
 import { MainHeaderComponent } from './components/main-header/main-header.component';
+import {MenubarModule} from 'primeng/menubar';
 
 
 
@@ -11,7 +12,8 @@ import { MainHeaderComponent } from './components/main-header/main-header.compon
     MainHeaderComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MenubarModule
   ]
 })
 export class MainPageModule { }
